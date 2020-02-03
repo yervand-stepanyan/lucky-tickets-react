4c96621ff60b4195baf327308f03109e4b1923a9
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-02-03 20:20:51 +0400

    Add LuckyTickets Component

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './input.css';
 import TextField from '@material-ui/core/TextField';
 
@@ -6,12 +6,13 @@ export default class Input extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {};
   }
 
   render() {
     return (
       <div className="inputMainDiv">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       </div>
     );
   }
---
+++ src/Components/LuckyTickets/LuckyTickets.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LuckyTickets() {
+  return <div className="mainDiv"></div>;
+}
