1cb13a9d7e3c7eccd248deaf99886798779ff6ca
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-02-03 22:50:48 +0400

    Updates

---
+++ precache-manifest.ff0197ffc1937c46d09ad1d68c8423ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a610e767097d2b8845afdba7a40671cd",
+    "url": "/lucky-tickets-react/index.html"
+  },
+  {
+    "revision": "5a92987990035fb3f0fb",
+    "url": "/lucky-tickets-react/static/css/main.7d290ee2.chunk.css"
+  },
+  {
+    "revision": "ca2d6882ff8c0b085767",
+    "url": "/lucky-tickets-react/static/js/2.ca6f4ebc.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/lucky-tickets-react/static/js/2.ca6f4ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a92987990035fb3f0fb",
+    "url": "/lucky-tickets-react/static/js/main.83a19ac1.chunk.js"
+  },
+  {
+    "revision": "183fb69e5ef5b05d98f1",
+    "url": "/lucky-tickets-react/static/js/runtime-main.bf7660db.js"
+  }
+]);
